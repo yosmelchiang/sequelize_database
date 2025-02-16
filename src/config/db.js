ae318201f15102d7af82b537ef8306ac43b78214
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-02-16 13:28:37 +0100

    tested locally, deploying to glitch.new

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,8 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 //Creates a new instance of Sequelize, stored in db
 const db = new Sequelize({
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
